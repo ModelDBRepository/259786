9a90b4d4d18eb3f80cf447ab99530080c7b48bb9
Tom Morse <tom.morse@yale.edu>
2019-10-15 15:08:56 -0400

    Added some screenshots and run hints to the readme

--- Matlab_scripts/traj_plotter.m
+++ Matlab_scripts/traj_plotter.m
@@ -4,10 +4,10 @@
 %S. Wittman, 6 October 2019
 
 file='4p-e.ode';
-sets = {'intact.set'};
+sets = {'4p-e_intact.set'};
 
 for i=1:length(sets)
-    success=RunXPP(file,sets{i});
+    success=RunXPP(file,sets{i}); %Formatting of this line is dependent on the version of RunXPP
     if success==0
         display('Failed to run');
         return
@@ -18,10 +18,10 @@ for i=1:length(sets)
     time=data(start_var:end,1);
     time = time; %If there is any transience, adjust for it by subtracting
 
-    v1=data(start_var:end,2);
-    v2=data(start_var:end,3);
-    v3=data(start_var:end,4);
-    v4=data(start_var:end,5);
+    v1=data(start_var:end,2); %The correct columns from output.dat must be specified
+    v2=data(start_var:end,6);
+    v3=data(start_var:end,9);
+    v4=data(start_var:end,12);
     
     a=figure;
     hold on
@@ -36,7 +36,8 @@ for i=1:length(sets)
     xlabel('Time (ms)');
     ylabel('(mV)');
     axis([0 5000 -80 0]);
-    title(sets(i).name);
+    % title(sets(i).name);
+    title(sets{i},'Interpreter','none');
     box off;
     set(a,'color','white');
 end
